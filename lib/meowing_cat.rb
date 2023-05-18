e956b08771bac2ff24ce86bd469a90f037eecdb8
lordish <gitonga403@gmail.com>
2023-05-18 13:23:20 +0300

    final commit

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
 ## code your solution here. 
+class Cat 
+    attr_accessor :meow, :name
+end
+
+maru = Cat.new
+maru.meow = "meow!"
+puts maru.meow
+
+cat_name = Cat.new
+cat_name.name = "Maru"
+puts cat_name.name
+
